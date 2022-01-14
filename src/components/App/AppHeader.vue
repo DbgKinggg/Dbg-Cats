35abb7e6148b29237af8142f05281220b8d2def8
DbgKing <dbgking@163.com>
2022-01-14 14:13:27 +1100

    ui change

--- src/components/App/AppHeader.vue
+++ src/components/App/AppHeader.vue
@@ -3,7 +3,9 @@
         class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box absolute top-5 w-11/12 md:w-4/5"
     >
         <div class="flex-1 px-2 mx-2">
-            <span class="text-lg font-bold">逗比国猫咪</span>
+            <span class="text-lg font-bold">
+                <a href="/">逗比国猫咪</a>
+            </span>
         </div>
         <div class="flex-none">
             <button class="btn btn-ghost">连接钱包</button>
--- src/components/App/AppMain.vue
+++ src/components/App/AppMain.vue
@@ -2,7 +2,7 @@
     <div class="text-center hero-content">
         <div class="max-w-md">
             <h1 class="mb-5 text-6xl font-bold tracking-widest">逗比国猫咪</h1>
-            <p class="mb-5">一个船新的免费随机生成NFT系列</p>
+            <p class="mb-5">一个船新的开源免费随机生成NFT系列</p>
             <button class="shadow-2xl lg:btn-lg btn btn-md btn-primary">铸造</button>
             <AppSocialMedia />
         </div>
